ed3cdee2e56351a844d82b164b804987dbcbd519
Roland Walker <walker@pobox.com>
2012-09-25 16:33:59 -0400

    work around package.el errors when upgrading package.el runs the byte-compiler, which notices whenever a function has been updated to take an additional parameter, opening a warnings window which is very confusing to the end user.

--- anaphora.el
+++ anaphora.el
@@ -386,7 +386,7 @@ DIVIDEND, DIVISOR, and DIVISORS are otherwise as documented for `/'."
 ;; mangle-whitespace: t
 ;; require-final-newline: t
 ;; coding: utf-8
-;; byte-compile-warnings: (not cl-functions)
+;; byte-compile-warnings: (not cl-functions redefine)
 ;; End:
 ;;
 ;; LocalWords: Anaphora EXPR awhen COND ARGS alambda ecase typecase
